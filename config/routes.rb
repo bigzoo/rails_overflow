7a355364781267e622043e212b164cd2b8348e46
Chris Nyaga <nyagachris75@gmail.com>
2017-08-08 13:06:18 +0300

    Get method for the questions upvote added to the router.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
+  get 'upvote', to: 'questions#upvote', as: 'upvote'
   delete 'logout', to: 'sessions#destroy', as: 'logout'
   root 'main#index'
   resources :users
