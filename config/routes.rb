1d61b7013c580dfcc5631d12a153a4f8583a7023
Chris Nyaga <nyagachris75@gmail.com>
2017-08-08 09:43:34 +0300

    Signup, logout and login route created to access the sessions routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
+  root 'main#index'
   resources :users
   resources :sessions
-  root 'main#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
