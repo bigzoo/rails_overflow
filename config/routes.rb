50ff9507157ad64d227732b790cfe56570a0dbfc
Chris Nyaga <nyagachris75@gmail.com>
2017-08-08 16:03:05 +0300

    Router updated with a get request for updating answers.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'upvote', to: 'questions#upvote', as: 'upvote'
+  get 'upvote_answer', to: 'answers#upvote', as: 'upvote_answer'
   delete 'logout', to: 'sessions#destroy', as: 'logout'
   root 'main#index'
   resources :users
